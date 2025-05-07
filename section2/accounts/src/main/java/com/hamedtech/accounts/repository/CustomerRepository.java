051f22f6d8c4a963eb9dc373e2285b2fb84544da
HamedMiri72 <hamedmiri72@gmail.com>
2025-05-07 23:02:44 +0100

    account fraimwork

---
+++ section2/accounts/src/main/java/com/hamedtech/accounts/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.hamedtech.accounts.repository;
+
+import com.hamedtech.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
