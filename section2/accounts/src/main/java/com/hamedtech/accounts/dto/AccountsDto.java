d47045c7b16737c2881ccfd5e76e51f94a5bf1a7
HamedMiri72 <hamedmiri72@gmail.com>
2025-05-08 22:37:46 +0100

    Dto entities

---
+++ section2/accounts/src/main/java/com/hamedtech/accounts/dto/AccountsDto.java
@@ -0,0 +1,13 @@
+package com.hamedtech.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+
+    private Long accountNumber;
+
+    private Long accountType;
+
+    private String branchAddress;
+}
