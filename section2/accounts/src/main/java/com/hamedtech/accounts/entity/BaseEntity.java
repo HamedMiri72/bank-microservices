35270343b02eb3749fcc334a4ce74abf3b76045d
HamedMiri72 <hamedmiri72@gmail.com>
2025-05-10 13:31:58 +0100

    update audit in database created at updated at created by updated by

--- section2/accounts/src/main/java/com/hamedtech/accounts/entity/BaseEntity.java
+++ section2/accounts/src/main/java/com/hamedtech/accounts/entity/BaseEntity.java
@@ -2,9 +2,15 @@ package com.hamedtech.accounts.entity;
 
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
 import org.hibernate.sql.Update;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -14,17 +20,22 @@ import java.time.LocalDateTime;
 @Setter
 @ToString
 @MappedSuperclass // is going to add super class for all entities
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
+    @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime updatedAt;
 
+    @LastModifiedBy
     @Column(insertable = false)
     private String updatedBy;
 
