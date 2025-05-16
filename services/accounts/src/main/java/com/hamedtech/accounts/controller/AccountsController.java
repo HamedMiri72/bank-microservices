1431df784b726a99fda6c91db1352247ed9678aa
HamedMiri72 <hamedmiri72@gmail.com>
2025-05-16 14:53:59 +0100

    accounts: build-info method to read property inside the application.yml file

--- services/accounts/src/main/java/com/hamedtech/accounts/controller/AccountsController.java
+++ services/accounts/src/main/java/com/hamedtech/accounts/controller/AccountsController.java
@@ -15,6 +15,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,13 +24,21 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
 @Validated
 @Tag(name = "CRUD REST APIs for Accounts in bank", description = "CRUD REST APIs for Accounts in bank")
 public class AccountsController {
 
 
-    private IAccountsService iAccountsService;
+    private final IAccountsService iAccountsService;
+
+    // This way we can autowire only the constructor with IAccountsService.
+    // Other fields will not be autowired unlike when using @AllArgsConstructor.
+    public AccountsController(IAccountsService iAccountsService) {
+        this.iAccountsService = iAccountsService;
+    }
+
+    @Value("${build.version}")
+    private String buildVersion;
 
     @Operation(summary = "Create Account REST API", description = "REST API to create Customer and Account inside bank")
     @ApiResponses({
@@ -118,6 +127,21 @@ public class AccountsController {
                     .status(HttpStatus.EXPECTATION_FAILED)
                     .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417));
         }
+    }
+
+    @Operation(summary = "Get Build Version REST API", description = "REST API to get Build Version inside bank")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status 200 OK"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status 500 INTERNAL SERVER ERROR",
+            content = @Content(
+                    schema = @Schema(implementation = ErrorResponseDto.class)
+            ))
+    })
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo(){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(buildVersion);
     }
 
 }
