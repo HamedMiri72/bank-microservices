eecf6bc496f84a3adf5ef7783d3f056fe5bc7a21
HamedMiri72 <hamedmiri72@gmail.com>
2025-05-18 00:12:13 +0100

    reading property from environmental variable in local system

--- services/cards/src/main/java/com/hamedTech/cards/controller/CardsController.java
+++ services/cards/src/main/java/com/hamedTech/cards/controller/CardsController.java
@@ -16,7 +16,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -34,6 +36,9 @@ public class CardsController {
         }
         @Value("${build.version}")
         private String buildVersion;
+
+        @Autowired
+        private Environment environment;
 
 
         @PostMapping("/create")
@@ -133,5 +138,20 @@ public class CardsController {
                 return ResponseEntity
                         .status(HttpStatus.OK)
                         .body(buildVersion);
+        }
+
+        @Operation(summary = "Get Java Version REST API", description = "REST API to get Java Version inside bank")
+        @ApiResponses({
+                @ApiResponse(responseCode = "200", description = "HTTP Status 200 OK"),
+                @ApiResponse(responseCode = "500", description = "HTTP Status 500 INTERNAL SERVER ERROR",
+                        content = @Content(
+                                schema = @Schema(implementation = ErrorResponseDto.class)
+                        ))
+        })
+        @GetMapping("/java-version")
+        public ResponseEntity<String> getJavaVersion(){
+                return ResponseEntity
+                        .status(HttpStatus.OK)
+                        .body(environment.getProperty("JAVA_HOME") + " " + environment.getProperty("MAVEN_HOME"));
         }
 }
