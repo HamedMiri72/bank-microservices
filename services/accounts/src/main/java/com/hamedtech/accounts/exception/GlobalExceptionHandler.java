38b7f205031b90470dd946df2082c5cc8a6fc043
HamedMiri72 <hamedmiri72@gmail.com>
2025-05-23 14:07:31 +0100

    regestring snd deregestring from eureka

--- services/accounts/src/main/java/com/hamedtech/accounts/exception/GlobalExceptionHandler.java
+++ services/accounts/src/main/java/com/hamedtech/accounts/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -72,4 +73,6 @@ public class GlobalExceptionHandler{
                 });
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
+
+
 }
