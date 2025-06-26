9e382e55bac30dd00c79d883b2013680399a7c84
HamedMiri72 <hamedmiri72@gmail.com>
2025-06-26 13:40:39 +0100

    updating gateway as a resourceserver with oauth2

---
+++ services/gatewayserver/src/main/java/com/hamedTech/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.hamedTech.gatewayserver.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
+
+                .pathMatchers("/bank/accounts/**").authenticated()
+                .pathMatchers("/bank/cards/**").authenticated()
+                .pathMatchers("/bank/loans/**").authenticated())
+                .oauth2ResourceServer(oAuth -> oAuth.jwt(Customizer.withDefaults()));
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+}
